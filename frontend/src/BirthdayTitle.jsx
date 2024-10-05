8319e959d66554b1f507df4670be80c0c5a18db1
CammoLammo <kamepoh2@gmail.com>
2024-10-05 17:51:38 +0800

    Push to Vercel

--- frontend/src/BirthdayTitle.jsx
+++ frontend/src/BirthdayTitle.jsx
@@ -16,7 +16,7 @@ function BirthdayTitle({ name, startDateTime }) {
         <h1 className="birthday-title">Happy Birthday</h1>
         <h1 className="birthday-title">{name}!</h1>
       </div>  
-      <CountdownTimer targetDate={playtimeEnd} color="#EB4781" timerName="Play Time" endText={`Play Time Over Kids can no longer play`} earlyText={`Not Started please wait for party host`} earlyCutoff={ 60 } />
+      <CountdownTimer targetDate={playtimeEnd} color="#EB4781" timerName="Play Time" endText={`Kids can no longer play`} earlyText={`Not Started please wait for party host`} earlyCutoff={ 60 } />
       <CountdownTimer targetDate={partyTimeEnd} color="#2CF6B3" timerName="Party Room" endText={`Party Time Over Head to desk to pay`} earlyText={`Set up time`} earlyCutoff= { 120 } />
     </div>
   );
